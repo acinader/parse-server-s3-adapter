9f186797b14b20cd5a4ea1ab050d5d9fd673ba5f
David Six <david.six@viawest.com>
2016-10-03 14:00:55 -0400

    Proper formatting

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function S3Adapter() {
     s3Options.secretAccessKey = options.secretKey;
   }
 
-  Object.assign(s3Options,options.s3overrides);
+  Object.assign(s3Options, options.s3overrides);
 
   this._s3Client = new AWS.S3(s3Options);
   this._hasBucket = false;
